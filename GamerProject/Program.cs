87f38019a2ab93ec494b4696012becede7b70d6f
Ümit Dengiz <umitdengiz98@gmail.com>
2021-05-11 14:17:16 +0300

    much

---
+++ GamerProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GamerProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer
+            {
+                Id = 1,
+                BirthYear = 1997,
+                FirstName = "Ümit",
+                LastName = "Dengiz",
+                IdentityNumber = 12345
+            });
+
+            Console.WriteLine();
+        }
+    }
+}
